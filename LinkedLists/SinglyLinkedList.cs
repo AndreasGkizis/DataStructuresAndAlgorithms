7b62dc45d21e3c48fdc22ffa417811be84e64081
Andreas Gkizis <75754279+AndreasGkizis@users.noreply.github.com>
2023-08-22 17:52:51 +0300

    initial

---
+++ LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace LinkedLists
+{
+    public class SinglyLinkedList<T>
+    {
+        public T Head { get { return _head.Value; } }
+        public T Tail { get { return _tail.Value; } }
+        // First Node
+        private SinglyLinkedListNode<T> _head { get; set; }
+        // Last Node
+        private SinglyLinkedListNode<T> _tail { get; set; }
+        public void AddHead(T item)
+        {
+            SinglyLinkedListNode<T> nodeToAdd = new SinglyLinkedListNode<T>(item);
+            nodeToAdd.Next = _head;
+            _head = nodeToAdd;
+            if (_tail == null)
+            {
+                _tail = nodeToAdd;
+            }
+        }
+
+        public void AddTail(T item)
+        {
+            SinglyLinkedListNode<T> nodeToAdd = new SinglyLinkedListNode<T>(item);
+            nodeToAdd.Next= _tail;
+            _tail = nodeToAdd;
+        }
+
+        public override string? ToString()
+        {
+            var sb =  new StringBuilder();
+            var currentNode = _head;
+
+            while (currentNode != null)
+            {
+                sb.Append(currentNode.Value);
+                if (currentNode.Next != null)
+                {
+                    sb.Append(" -> ");
+                }
+                currentNode = currentNode.Next;
+            }
+            return sb.ToString();
+        }
+    }
+
+    /// <summary>
+    /// This will be every Node in my Linked List, the only thing it knows is its value and the next node.
+    /// </summary>
+    /// <typeparam name="T">The type of value stored in the node.</typeparam>
+    public class SinglyLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public SinglyLinkedListNode<T> Next { get; set; }
+        public SinglyLinkedListNode(T value, SinglyLinkedListNode<T> nextNode = null)
+        {
+            Value = value;
+            Next = nextNode;
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,39 @@
+﻿using LinkedLists;
+
+namespace Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new SinglyLinkedListNode<string>("node 1");
+            var node2 = new SinglyLinkedListNode<string>("node 2");
+            var node3 = new SinglyLinkedListNode<string>("node 3");
+            var node4 = new SinglyLinkedListNode<string>("node 4");
+
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+
+            var test = new SinglyLinkedList<string>();
+
+            test.AddHead("node #1");
+            test.AddHead("node #2");
+            test.AddHead("node #3");
+            test.AddHead("node #4");
+            test.AddHead("node #5");
+            test.AddHead("node #6");
+            test.AddHead("node #7");
+            test.AddTail("node #0");
+            test.AddTail("node #-1");
+            test.AddTail("node #-2");
+
+            Console.WriteLine(  
+            test.ToString()
+                
+                );
+
+            //Console.WriteLine("end");
+        }
+    }
+}
